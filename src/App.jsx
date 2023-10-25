e72d069e8ace646afac229a907f91e781c2d04c6
farhan-71 <farhanmazumder75@gmail.com>
2023-10-25 09:57:41 +0600

    Implement a dark mode feature in this small project using React hooks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import useTheme from "./hooks/useTheme";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [darkMode, setDarkMode] = useState(false);
+  // const html = document.documentElement;
+  // // console.log(html);
+  // const handleThemeChange = () => {
+  //   if (!darkMode) {
+  //     html.classList.remove("dark");
+  //     html.classList.add("light");
+  //     setDarkMode(!darkMode);
+  //     localStorage.setItem("darkMode", true);
+  //   } else {
+  //     html.classList.remove("light");
+  //     html.classList.add("dark");
+  //     localStorage.setItem("darkMode", false);
+  //     setDarkMode(!darkMode);
+  //   }
+  // };
+  // useEffect(() => {
+  //   const currentMode = localStorage.getItem("darkMode") || false;
+  //   console.log(currentMode);
+  //   if (currentMode) {
+  //     html.classList.remove("light");
+  //     html.classList.add("dark");
+  //   } else {
+  //     html.classList.remove("dark");
+  //     html.classList.add("light");
+  //   }
+  // }, []);
+  // darkMode ? (
+  //   html.classList.remove("light")
+  //   html.classList.add("dark")
+  //   ) : (
+
+  //   )
+
+  // const [mode, setMode] = useState("light");
+  // const html = document.documentElement;
+  // const handleThemeChange = () => {
+  //   if (mode === "light") {
+  //     html.classList.remove("light");
+  //     html.classList.add("dark");
+  //     setMode("dark");
+  //     localStorage.setItem("mode", "dark");
+  //   } else {
+  //     html.classList.remove("dark");
+  //     html.classList.add("light");
+  //     setMode("light");
+  //     localStorage.setItem("mode", "light");
+  //   }
+  // };
+  // useEffect(() => {
+  //   const currentMode = localStorage.getItem("mode") || "light";
+  //   setMode(currentMode);
+  //   // html.classList.add(currentMode)
+  //   document.documentElement.classList.add(currentMode);
+  // }, []);
+
+  const { changeTheme, mode } = useTheme();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="h-screen dark:bg-slate-800">
+      <div className="max-w-sm py-20 mx-auto space-y-5 dark:text-white ">
+        <h1 className="text-4xl font-bold text-center">Hello world!</h1>
+        <p className="p-5 rounded-sm dark:bg-slate-700 bg bg-slate-200">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, quam,
+          eaque repellat pariatur tenetur id adipisci mollitia dolores soluta
+          nesciunt quaerat corrupti fugiat a accusantium atque perferendis
+          quisquam assumenda obcaecati!
         </p>
+        <button
+          onClick={changeTheme}
+          className="block px-3 py-1 mx-auto text-lg font-medium text-center text-white rounded dark:bg-slate-400 dark:text-black bg-sky-400"
+        >
+          Make {mode === "light" ? "Dark" : "Light"}
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/hooks/useTheme.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useTheme = () => {
+  const [mode, setMode] = useState("light");
+  const html = document.documentElement;
+  const changeTheme = () => {
+    if (mode === "light") {
+      html.classList.remove("light");
+      html.classList.add("dark");
+      setMode("dark");
+      localStorage.setItem("mode", "dark");
+    } else {
+      html.classList.remove("dark");
+      html.classList.add("light");
+      setMode("light");
+      localStorage.setItem("mode", "light");
+    }
+  };
+  useEffect(() => {
+    const currentMode = localStorage.getItem("mode") || "light";
+    setMode(currentMode);
+    // html.classList.add(currentMode)
+    document.documentElement.classList.add(currentMode);
+  }, []);
+  return { changeTheme, mode };
+};
+
+export default useTheme;
